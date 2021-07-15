71b40061724c7932147ac7d741f3269ada5a1400
Courssumm02 <Courssumm02@ic.intranet.epfl.ch>
2021-07-15 15:25:43 +0200

    M7

--- Module Extra 7/app.js
+++ Module Extra 7/app.js
@@ -9,11 +9,11 @@ var frame_objects=[]
 var all_connecters=[]
 var all_tops=[]
 var all_bottoms=[]
-var len = 200
+var len = 250
 for (let i = 0; i < len; i++){
-    var pos_x = -len+i*30
+    var pos_x = -len+i*30 -40
     var x = Math.PI/2*i/3
-    var dist = 80
+    var dist = 60
     var up = Math.round(Math.sin(x)*dist)
     var down = -Math.round(Math.sin(x)*dist)
     var connecter = two.makeRectangle(pos_x,300,10,up*2)
@@ -22,14 +22,14 @@ for (let i = 0; i < len; i++){
     connecter.linewidth=3
     var size1 = 10//(up/40 + 10)/1
     var size2 = 10//(down/40 + 10)/1
-    var circle_top = two.makeCircle(pos_x, 300 + up, size1)
-    circle_top.fill = "red"
-    circle_top.stroke = "darkred"
-    circle_top.linewidth = 3
     var circle_bottom = two.makeCircle(pos_x, 300 + down, size2)
     circle_bottom.fill = "orange"
     circle_bottom.stroke = "#cc5500"
     circle_bottom.linewidth = 3
+    var circle_top = two.makeCircle(pos_x, 300 + up, size1)
+    circle_top.fill = "red"
+    circle_top.stroke = "darkred"
+    circle_top.linewidth = 3
     // frame_objects.push(connecter, circle_top, circle_bottom)
     all_connecters.push(connecter)
     all_tops.push(circle_top)
@@ -37,17 +37,17 @@ for (let i = 0; i < len; i++){
 }
 // var group = two.makeGroup(frame_objects)
 
-tops = two.makeGroup(all_tops)
 connecters = two.makeGroup(all_connecters)
+tops = two.makeGroup(all_tops)
 bottoms = two.makeGroup(all_bottoms)
 console.log(connecters.getBoundingClientRect());
 var n = 1
 var target=12
 two.bind('update', function(frameCount) {
-    if (frameCount%5==0){
+    if (frameCount%10==0){
+        connecters.translation.x+=n%target?30:-target*30
         tops.translation.x+=n%target?30:-target*30
         bottoms.translation.x+=n%target?30:-target*30
-        connecters.translation.x+=n%target?30:-target*30
         // console.log(n);
         n++
         if (n==target+1){
---
+++ README.md
@@ -0,0 +1,8 @@
+# Nature, In Code
+
+This a repo contains the projects and code for the summer camp in Laussane.
+
+## Photos:
+
+### Module 1:
+![Module 1 Image](./Images/md1_1.PNG)
